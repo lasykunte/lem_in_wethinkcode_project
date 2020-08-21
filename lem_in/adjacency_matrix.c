4c17df487ce6e2ecf48dfcfa0dc2a0a640877049
lasykunte <lasyramovha53@gmail.com>
2020-08-21 19:18:57 +0200

    lem-in project update

---
+++ lem_in/adjacency_matrix.c
@@ -0,0 +1,45 @@
+#include "lem_in.h"
+
+static void     valid_edge_link(f_graph_node *ptr_gr, char *link)
+{
+	char **room_edge;
+
+	room_edge = ft_strsplit(link, '-');
+	if (room_edge[2] != NULL)
+    {
+		free_arr_mem(room_edge, ptr_gr, 1);
+    }
+	free_arr_mem(room_edge, ptr_gr, 0);
+}
+
+void		adjacency_matrix(f_graph_node *ptr_gr)
+{
+	int		index;
+	int		row_order_i;
+	int		row_order_j;
+	char	**room_l;
+	char	**room_links;
+
+	index = -1;
+	room_links = ft_strsplit(ptr_gr->edge_links, '\n');
+	while (room_links[++index])
+	{
+		if (room_links[index][0] == '#')
+		{
+			continue ;
+		}
+		valid_edge_link(ptr_gr, room_links[index]);
+		room_l = ft_strsplit(room_links[index], '-');
+        row_order_i = room_vertex_indx(ptr_gr, room_l[0], 1);
+        row_order_j = room_vertex_indx(ptr_gr, room_l[1], 1);
+		if (row_order_i >= ptr_gr->room_count || row_order_j >= ptr_gr->room_count)
+		{
+			free_array(room_l, ptr_gr, 0);
+			free_array(room_links, ptr_gr, 1);
+		}
+		(row_order_j) ? (ptr_gr->adjacency_matrix_table[row_order_i][row_order_j] = 1) : 0;
+		(row_order_i) ? (ptr_gr->adjacency_matrix_table[row_order_j][row_order_i] = 1) : 0;
+		free_array(room_l, ptr_gr, 0);
+	}
+	free_array(room_links, ptr_gr, 0);
+}
--- lem_in/combine_ant_string.c
+++ lem_in/combine_ant_string.c
@@ -4,7 +4,7 @@ char	*combine_ant_string(char *str, char *str1, int cln)
 {
 	char *combined_str;
 
-	combined_str= ft_strnew(ft_strlen(str) + ft_strlen(str1) + 1);
+	combined_str = ft_strnew(ft_strlen(str) + ft_strlen(str1) + 1);
 	if (str[0] == 0)
     {
 		return (str1);
@@ -14,6 +14,8 @@ char	*combine_ant_string(char *str, char *str1, int cln)
 	ft_strcat(combined_str, str1);
 	ft_strdel(&str);
     if (cln)
+	{
         ft_strdel(&str1);
+	}
 	return (combined_str);
 }
