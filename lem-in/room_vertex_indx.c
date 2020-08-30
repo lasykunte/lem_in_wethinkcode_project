531de512eb555151874d979041f75edbc9f385f4
lasykunte <lasyramovha53@gmail.com>
2020-08-30 23:53:53 +0200

    working lem-n project update

--- lem-in/room_vertex_indx.c
+++ lem-in/room_vertex_indx.c
@@ -9,7 +9,9 @@ int		room_vertex_indx(f_graph_node *ptr_gr, char *room_vertex_name, int begin)
 		i = -1;
 	}
 	else
+	{
 		i = 0;
+	}
 	while (ptr_gr->room_vertex[++i] && i < (ptr_gr->room_count))
 	{  
 		if (ft_strcmp(ptr_gr->room_vertex[i], room_vertex_name) == 0)
